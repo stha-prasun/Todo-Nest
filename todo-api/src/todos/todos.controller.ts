afd25e58bb4cd618a1661ba19983c69c7e266e20
Prasun Shrestha <shresthaprasun88@gmail.com>
2026-06-26 11:38:18 +0545

    Add todo

--- todo-api/src/todos/todos.controller.ts
+++ todo-api/src/todos/todos.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+} from '@nestjs/common';
+import { TodosService } from './todos.service';
+import { CreateTodoDto } from './dto/create-todo.dto';
 
 @Controller('todos')
-export class TodosController {}
+export class TodosController {
+
+  constructor(
+    private readonly todoService: TodosService,
+  ) {}
+
+  @Post()
+  create(
+    @Body() dto: CreateTodoDto,
+  ) {
+    return this.todoService.create(dto);
+  }
+
+}
--- todo-api/src/todos/todos.service.ts
+++ todo-api/src/todos/todos.service.ts
@@ -2,11 +2,16 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Todo } from './schemas/todo.schema';
 import { Model } from 'mongoose';
+import { CreateTodoDto } from './dto/create-todo.dto';
 
 @Injectable()
 export class TodosService {
-    constructor(
-        @InjectModel(Todo.name)
-        private todoModel: Model<Todo>,
-    ) {}
+  constructor(
+    @InjectModel(Todo.name)
+    private todoModel: Model<Todo>,
+  ) {}
+
+  async create(dto: CreateTodoDto) {
+    return this.todoModel.create(dto);
+  }
 }
